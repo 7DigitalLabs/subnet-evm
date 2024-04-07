0e2cf6db76054650e835a85cba8e8da2253c815c
7 Digital Labs <tech@7digilabs.com>
2024-04-07 22:00:14 +0200

    Update state_processor.go

--- core/state_processor.go
+++ core/state_processor.go
@@ -122,7 +122,6 @@ func applyTransaction(msg *Message, config *params.ChainConfig, gp *GasPool, sta
 
 	// Apply the transaction to the current state (included in the env).
 	result, err := ApplyMessage(evm, msg, gp)
-	log.Info("RESULT", "FAILED:", result.Failed())
 	if err != nil {
 		return nil, err
 	}
